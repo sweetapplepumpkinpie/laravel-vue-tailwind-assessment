c66afd767a22f8fe2078deb29949519132dad5d5
sweetapplepumpkinpie <sweetapplepumpkinpie@gmail.com>
2023-03-29 11:52:25 -0400

    added contact seeder to create 50 dummy contacts

---
+++ database/factories/ContactFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Contact;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Contact>
+ */
+class ContactFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+
+    protected $model = Contact::class;
+
+    public function definition()
+    {
+        return [
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'phone' => $this->faker->phoneNumber,
+        ];
+    }
+}
